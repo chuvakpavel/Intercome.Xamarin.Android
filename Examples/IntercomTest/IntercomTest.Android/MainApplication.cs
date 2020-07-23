f05590528bc2384154e7be684d5c700924a88c3a
chuvakpavel <chuvak.pavel@gmail.com>
2020-07-23 10:54:15 +0300

    Add Intercom examples

---
+++ Examples/IntercomTest/IntercomTest.Android/MainApplication.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using IO.Intercom.Android;
+using IO.Intercom.Android.Identity;
+
+namespace IntercomTest.Droid
+{
+    [Application]
+    public class MainApplication : Application, Application.IActivityLifecycleCallbacks
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership transer)
+          : base(handle, transer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            RegisterActivityLifecycleCallbacks(this);
+            //A great place to initialize Xamarin.Insights and Dependency Services!
+
+            Intercom.Initialize(this, "your api key", "your app id");
+            Registration registration = Registration.Create().WithUserId("123456");
+            Intercom.Client().RegisterIdentifiedUser(registration);
+            Intercom.Client().DisplayMessenger();
+        }
+
+        public override void OnTerminate()
+        {
+            base.OnTerminate();
+            UnregisterActivityLifecycleCallbacks(this);
+        }
+
+        public void OnActivityCreated(Activity activity, Bundle savedInstanceState)
+        {
+        }
+
+        public void OnActivityDestroyed(Activity activity)
+        {
+        }
+
+        public void OnActivityPaused(Activity activity)
+        {
+        }
+
+        public void OnActivityResumed(Activity activity)
+        {
+        }
+
+        public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
+        {
+        }
+
+        public void OnActivityStarted(Activity activity)
+        {
+        }
+
+        public void OnActivityStopped(Activity activity)
+        {
+        }
+    }
+}
---
+++ Examples/IntercomTest/IntercomTest.iOS/AppDelegate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace IntercomTest.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        // class-level declarations
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            Intercom.iOS.Intercom.SetApiKey("your api key", "your app id");
+
+            Intercom.iOS.Intercom.RegisterUnidentifiedUser();
+            Intercom.iOS.Intercom.PresentConversationList();
+
+            return true;
+        }
+
+        public override UIInterfaceOrientationMask GetSupportedInterfaceOrientations(UIApplication application, UIWindow forWindow)
+        {
+            return UIInterfaceOrientationMask.Portrait;
+        }
+    }
+}
